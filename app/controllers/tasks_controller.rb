8628363fcf751d0323a32eddcac86199429738e5
yasukouchi <saladoil55@gmail.com>
2020-01-17 06:35:13 +0000

    task-list2 add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,10 +16,10 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Message が正常に投稿されました'
+      flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Message が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
@@ -29,17 +29,17 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      flash[:success] = 'Message は正常に更新されました'
+      flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
+      flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
     @task.destroy
-    flash[:success] = 'Message は正常に削除されました'
+    flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
     private
