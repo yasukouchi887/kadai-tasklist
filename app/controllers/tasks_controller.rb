333696e640acbebf7bdc1fc66f212c8012ad2d39
yasukouchi <saladoil55@gmail.com>
2020-02-06 13:32:59 +0000

    kadai-tasklist5 post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
     if logged_in?
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
     end
   end
